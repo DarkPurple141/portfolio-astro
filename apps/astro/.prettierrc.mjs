167f0b988a58553fdacaf2b20cab2f11c2a5ae8b
Alex Hinds <alex.hinds141@gmail.com>
2023-11-20 21:52:53 +1100

    chore: got there in the end, create shareable conifg as well

--- apps/astro/.prettierrc.mjs
+++ apps/astro/.prettierrc.mjs
@@ -1,7 +1,9 @@
+import config from '@halyard/prettier-config'
+
 /** @type {import("prettier").Config} */
 export default {
+  ...config,
   plugins: ['prettier-plugin-astro'],
-  semi: false,
   overrides: [
     {
       files: '*.astro',
@@ -10,4 +12,4 @@ export default {
       },
     },
   ],
-};
+}
